56c671a8b173c86fe409fa4e806e013c129c3b6e
José Gamaliel Rivera Ibarra <jgrivera@novutek.com>
2018-09-21 22:23:16 -0700

    First attempt to smooth turn on and off

--- src/main.c
+++ src/main.c
@@ -17,6 +17,7 @@
 
 #include "mgos.h"
 #include "mgos_time.h"
+#include "mgos_timers.h"
 #include "mgos_gpio.h"
 #include "mgos_dht.h"
 #include "mgos_adc.h"
@@ -24,21 +25,115 @@
 #include "mgos_mqtt.h"
 
 static struct mgos_dht *s_dht = NULL;
+// static mgos_timer_id effect_timer = MGOS_INVALID_TIMER_ID;
+static mgos_timer_id smooth_timer = MGOS_INVALID_TIMER_ID;
+static float last_motion_time = 0;
+static int smooth_brightness = 0;
+
+// TODO: static void get_weather(char *)
+
+/*
+static void clear_effect_timer() {
+  if(effect_timer != MGOS_INVALID_TIMER_ID) {
+    mgos_clear_timer(effect_timer);
+    effect_timer = MGOS_INVALID_TIMER_ID;
+  }
+}
+*/
+
+static int get_luminosity() {
+  if(mgos_adc_enable(0)) {
+    return mgos_adc_read(0);
+  }
+  LOG(LL_INFO, ("ADC not available"));
+  return 0;
+}
+
+static bool is_dark() {
+  return get_luminosity() <= mgos_sys_config_get_pir_threshold();
+}
+
+static void bled_timer_cb(void *args) {
+  mgos_gpio_toggle(mgos_sys_config_get_pins_bled());
+  (void) args;
+}
 
 static void dht_timer_cb(void *dht) {
-  int t = mgos_dht_get_temp(dht);
-  int h = mgos_dht_get_humidity(dht);
-  int l = mgos_adc_read(0);
+  float t = mgos_dht_get_temp(dht);
+  float h = mgos_dht_get_humidity(dht);
+  int l = get_luminosity();
 
   if (isnan(h) || isnan(t)) {
     LOG(LL_INFO, ("Failed to read data from sensor\n"));
     return;
   }
-  LOG(LL_INFO, ("\nTemperature: %d *C \nHumidity: %d %%\nLuminosity: %d lux", t, h, l));
+  LOG(LL_INFO, ("\nTemperature: %d *C \nHumidity: %d %%\nLuminosity: %d lux", (int)t, (int)h, l));
 
-  mgos_mqtt_pubf("Weather", 1, false, "{temp:%d,hum:%d,lum:%d,uptime:%f}", t, h, l, mgos_uptime());
+  mgos_mqtt_pubf("Weather", 1, false, "{temp:%d,hum:%d,lum:%d,uptime:%f}", (int)t, (int)h, l, mgos_uptime());
 
   (void) dht;
+}
+
+static void led_strip_brightness(int brightness) {
+  LOG(LL_INFO, ("Set Led Strip Brightness: %d", brightness));
+}
+
+static void smooth_turn_off() {
+  smooth_brightness -= 50;
+  if(smooth_brightness > 0) {
+    led_strip_brightness(smooth_brightness);
+  } else {
+    if(smooth_timer != MGOS_INVALID_TIMER_ID) {
+      mgos_clear_timer(smooth_timer);
+      smooth_timer = MGOS_INVALID_TIMER_ID;
+    }
+    // TODO: turn_off
+  }
+}
+
+static void check_last_motion_time() {
+  LOG(LL_INFO, ("Check last motion time"));
+  int diff = mgos_uptime() - last_motion_time;
+  if (diff < mgos_sys_config_get_pir_keep()) {
+    int wait = mgos_sys_config_get_pir_keep() - diff;
+    mgos_set_timer(wait, false, check_last_motion_time, NULL);
+  } else {
+    if(smooth_timer != MGOS_INVALID_TIMER_ID) {
+      mgos_clear_timer(smooth_timer);
+    }
+    smooth_timer = mgos_set_timer(1000, true, smooth_turn_off, NULL);
+  }
+}
+
+static void smooth_turn_on() {
+  LOG(LL_INFO, ("Smooth turn ON"));
+  smooth_brightness += 50;
+  if(smooth_brightness <= 250) {
+    led_strip_brightness(smooth_brightness);
+  } else {
+    if (smooth_timer != MGOS_INVALID_TIMER_ID) {
+      mgos_clear_timer(smooth_timer);
+      smooth_timer = MGOS_INVALID_TIMER_ID;
+    }
+    int wait = mgos_sys_config_get_pir_keep() * 1000;
+    smooth_timer = mgos_set_timer(wait, true, check_last_motion_time, NULL);
+  }
+}
+
+static void motion_handler() {
+  last_motion_time = mgos_uptime();
+  switch(mgos_sys_config_get_app_mode()) {
+    case 3:
+      if(is_dark() && smooth_timer == MGOS_INVALID_TIMER_ID) {
+        smooth_timer = mgos_set_timer(1000, true, smooth_turn_on, NULL);
+      }
+      break;
+    case 4:
+      LOG(LL_INFO, ("ALERT"));
+      break;
+    default:
+      LOG(LL_INFO, ("Ignore motion"));
+  }
 }
 
 static void pir_timer_cb(void *args) {
@@ -48,21 +143,21 @@ static void pir_timer_cb(void *args) {
   }
   if(motion) {
     LOG(LL_INFO, ("[%f] Motion detected", mgos_uptime()));
-    // TODO: call motion handler
+    motion_handler();
   }
   (void) args;
 }
 
 static void rpc_weather_cb(struct mg_rpc_request_info *ri, const char *args,
                            const char *src, void *dht) {
-  int t = mgos_dht_get_temp(dht);
-  int h = mgos_dht_get_humidity(dht);
-  int l = mgos_adc_read(0);
+  float t = mgos_dht_get_temp(dht);
+  float h = mgos_dht_get_humidity(dht);
+  int l = get_luminosity();
   if(isnan(h) || isnan(t)) {
     mg_rpc_send_errorf(ri, -1, "{error: \"Failed to read data from sensor\"");
   } else {
-    LOG(LL_INFO, ("\nTemperature: %d *C \nHumidity: %d %%\nLuminosity: %d lux", t, h, l));
-    mg_rpc_send_responsef(ri, "{temp:%d,hum:%d,lum:%d,uptime:%f}", t, h, l, mgos_uptime());
+    LOG(LL_INFO, ("\nTemperature: %d *C \nHumidity: %d %%\nLuminosity: %d lux", (int)t, (int)h, l));
+    mg_rpc_send_responsef(ri, "{temp:%d,hum:%d,lum:%d,uptime:%f}", (int)t, (int)h, l, mgos_uptime());
   }
   (void) src;
   (void) args;
@@ -70,17 +165,21 @@ static void rpc_weather_cb(struct mg_rpc_request_info *ri, const char *args,
 
 enum mgos_app_init_result mgos_app_init(void) {
 
+  // Configure Built in LED
+  mgos_gpio_set_mode(mgos_sys_config_get_pins_bled(), MGOS_GPIO_MODE_OUTPUT);
+  mgos_set_timer(1000, true, bled_timer_cb, NULL);
+
   // Configure DHT sensor
   s_dht = mgos_dht_create(mgos_sys_config_get_pins_dht(), DHT11);
   mgos_set_timer(mgos_sys_config_get_app_tele() * 1000, true, dht_timer_cb, s_dht);
 
   // Configure PIR sensor
-  mgos_gpio_set_mode(mgos_sys_config_get_pins_pir(), MGOS_GPIO_MODE_INPUT); // mgos_gpio_mode, return bool
+  mgos_gpio_set_mode(mgos_sys_config_get_pins_pir(), MGOS_GPIO_MODE_INPUT); // return bool
   mgos_gpio_set_mode(mgos_sys_config_get_pins_led(), MGOS_GPIO_MODE_OUTPUT);
   mgos_set_timer(500, true, pir_timer_cb, NULL);
 
   // Configure RPC interface
-  mgos_rpc_add_handler("Weather", rpc_weather_cb, s_dht);
+  mgos_rpc_add_handler("Driver.GetWeather", rpc_weather_cb, s_dht);
 
   return MGOS_APP_INIT_SUCCESS;
 }
